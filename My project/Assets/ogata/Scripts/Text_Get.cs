190ca223243eac26dd9ed9c1d1003010ba0037b5
Rei1729 <wxkk1i4656hb@gmail.com>
2024-02-07 01:05:47 +0900

    Result Sceneでスコア等出るように

---
+++ My project/Assets/ogata/Scripts/Text_Get.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Text_Get : MonoBehaviour
+{
+    ScoreManager tg_ScoreManager;
+
+    [Header("シーン名を表示するテキスト")]
+    [SerializeField] TextMeshProUGUI sceneName;
+    [Header("クリアタイムを表示するテキスト")]
+    [SerializeField] TextMeshProUGUI clearTimeValue;
+    [Header("最高クリアタイムを表示するテキスト")]
+    [SerializeField] TextMeshProUGUI bestClearTimeValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tg_ScoreManager = new ScoreManager();
+
+        sceneName.text = tg_ScoreManager.SM_getSceneName();
+        clearTimeValue.text = tg_ScoreManager.SM_getCurrentScore().ToString();
+        bestClearTimeValue.text = tg_ScoreManager.SM_getBestScore().ToString();
+    }
+
+}
