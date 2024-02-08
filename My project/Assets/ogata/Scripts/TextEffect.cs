b5f6ca212c33338793db6869762ead38f6189bcd
Rei1729 <wxkk1i4656hb@gmail.com>
2024-02-08 17:58:39 +0900

    リザルトのクリアタイムのアウトラインの虹色か実装

--- My project/Assets/ogata/Scripts/TextEffect.cs
+++ My project/Assets/ogata/Scripts/TextEffect.cs
@@ -24,6 +24,37 @@ public class TextEffect : MonoBehaviour
     /// </summary>
     const float magnificationValue = 0.0025f;
 
+    [Header("色変更スパン")]
+    public float change_Color_Time = 0.1f;
+
+    [Header("変更の滑らかさ")]
+    public float Smooth = 0.01f;
+
+    [Header("色彩")]
+    [Range(0, 1)] public float HSV_Hue = 1.0f;
+
+    [Header("彩度")]
+    [Range(0, 1)] public float HSV_Saturation = 1.0f;
+
+    [Header("明度")]
+    [Range(0, 1)] public float HSV_Brightness = 1.0f;
+
+    [Header("色彩：Max")]
+    [Range(0, 1)] public float HSV_Hue_max = 1.0f;
+
+    [Header("色彩：Min")]
+    [Range(0, 1)] public float HSV_Hue_min = 0.0f;
+
+
+    private void Start()
+    {
+        HSV_Hue = HSV_Hue_min;
+
+        // クリアタイムをゲーミングインコか
+        StartCoroutine("Change_Color");
+    }
+
+
     // Update is called once per frame
     void Update()
     {
@@ -51,30 +82,25 @@ public class TextEffect : MonoBehaviour
                 scaleSizeRota = true;
             }
         }
+    }
 
-        if(colorSpeedRota)
+    /// <summary>
+    /// ニューレコード更新したときにクリアタイムのアウトラインがゲーミング化する処理
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator Change_Color()
+    {
+        HSV_Hue += Smooth;
+
+        if(HSV_Hue >= HSV_Hue_max)
         {
-            if (clearTimeValue.effectColor.r < 255)
-            {
-                clearTimeValue.effectColor += Color.HSVToRGB(0.1f, 0.1f, 0.1f);
-                bestClearTimeValue.effectColor += Color.HSVToRGB(0.1f, 0.1f, 0.1f);
-            }
-            if (clearTimeValue.effectColor.r == 255)
-            {
-                colorSpeedRota = false;
-            }
+            HSV_Hue = HSV_Hue_min;
         }
-        else
-        {
-            if(clearTimeValue.effectColor.r > 0) 
-            {
-                clearTimeValue.effectColor -= Color.HSVToRGB(0.1f, 0.1f, 0.1f);
-                bestClearTimeValue.effectColor -= Color.HSVToRGB(0.1f, 0.1f, 0.1f);
-            }
-            if(clearTimeValue.effectColor.r == 0)
-            {
-                colorSpeedRota = true;
-            }
-        }
+
+        clearTimeValue.effectColor = Color.HSVToRGB(HSV_Hue, HSV_Saturation, HSV_Brightness);
+
+        yield return new WaitForSeconds(change_Color_Time);
+
+        StartCoroutine("Change_Color");
     }
 }
